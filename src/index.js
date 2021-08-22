6f64c98921996745513d8c6d983e5f699499c512
Aliaksei <avkomosko@gmail.com>
2021-08-22 20:48:00 +0300

    feat: implement function and finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let openedBrackets = [];
+  let closedBrackets = [];
+  let bracketsStack = [];
+  let lastItem;
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    openedBrackets.push(bracketsConfig[i][0]);
+    closedBrackets.push(bracketsConfig[i][1]);
+  }
+  for (let i = 0; i < str.length; i++) {
+    if (closedBrackets.includes(str[i]) && bracketsStack.length === 0 && !openedBrackets.includes(str[i])) {
+      return false;
+    } else if (openedBrackets.includes(str[i]) && !closedBrackets.includes(str[i])) {
+      bracketsStack.push(str[i]);
+    } else if (openedBrackets.includes(str[i]) && closedBrackets.includes(str[i])) {
+        if (!bracketsStack.includes(str[i])){
+          bracketsStack.push(str[i]);
+        } else {
+          bracketsStack.pop();
+        }
+    } else if (closedBrackets.includes(str[i]) && bracketsStack.length > 0) {
+      lastItem = bracketsStack[bracketsStack.length-1];
+      if (openedBrackets.indexOf(lastItem) === closedBrackets.indexOf(str[i])) {
+        bracketsStack.pop();
+      } else {
+        return false;
+      }
+    } 
+  }
+  return bracketsStack.length === 0;
 };
